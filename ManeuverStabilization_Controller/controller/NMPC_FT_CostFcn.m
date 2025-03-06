efd0319ce71bc5ab2f63a13bd4d55c0f20d18f95
aero309 <khanhky@outlook.com>
2025-03-06 14:38:17 +0100

    Added cost function

---
+++ ManeuverStabilization_Controller/controller/NMPC_FT_CostFcn.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = nmpc_costfcn(inputArg1,inputArg2)
+%NMPC_COSTFCN Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
