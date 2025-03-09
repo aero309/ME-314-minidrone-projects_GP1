4b2619985c7657739e59d2af2edb280d6c6cfee8
aero309 <khanhky@outlook.com>
2025-03-09 20:52:10 +0100

    Added new controller method

--- ManeuverStabilization_Geometric_tracking/controller/StateFcn.m
+++ ManeuverStabilization_Geometric_tracking/controller/StateFcn.m
@@ -1,7 +1,7 @@
-function [outputArg1,outputArg2] = untitled2(inputArg1,inputArg2)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function dxdt = StateFcn(x, u)
+    A = [0, 1, 0, 0; ...
+        0, 0, 0, 0; ...
+        0, 0, 0, 1; ...
+        0, 0, 0, 0];
+    B =
 end
-
