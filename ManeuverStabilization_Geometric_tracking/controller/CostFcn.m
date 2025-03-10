f3598e410cdad73a72d6c3ff2842eb259fe57c69
aero309 <khanhky@outlook.com>
2025-03-10 23:54:36 +0100

    tuning

--- ManeuverStabilization_Geometric_tracking/controller/CostFcn.m
+++ ManeuverStabilization_Geometric_tracking/controller/CostFcn.m
@@ -1,9 +1,9 @@
 function J = CostFcn(X, U, e, data)
-    Ts = 0.05;
+    Ts = 0.005;
     g = 9.81;
     N = data.PredictionHorizon;
     QN = diag([1, 0, 1, 0]);
-    z_offset = 0.5 * g * (Ts * N) ^ 2;
+    z_offset = - 0.5 * g * (Ts * N) ^ 2;
     x_ref = data.References(end, :);
     x_ref(3) = x_ref(3) + z_offset;
     errN = X(N, :) - x_ref;
--- ManeuverStabilization_Geometric_tracking/controller/NMPC.m
+++ ManeuverStabilization_Geometric_tracking/controller/NMPC.m
@@ -3,14 +3,15 @@ ny = 4;
 nu = 1;
 nlobj = nlmpc(nx,ny,nu);
 
-nlobj.Ts = 0.05;
-nlobj.PredictionHorizon = 20;
+nlobj.Ts = 0.005;
+nlobj.PredictionHorizon = 30;
 nlobj.ControlHorizon = 2;
 
 nlobj.Model.StateFcn = "StateFcn";
 nlobj.Optimization.CustomCostFcn = "CostFcn";
+nlobj.Model.IsContinuousTime = false;
 
 nlobj.MV(1).Min = (Vehicle.Motor.minLimit/Vehicle.Motor.thrustToMotorCommand) * 4;
-nlobj.MV(1).Max = (Vehicle.Motor.maxLimit/Vehicle.Motor.thrustToMotorCommand) * 4;
+nlobj.MV(1).Max = 0.8;
 
 validateFcns(nlobj,rand(nx,1),rand(nu,1));
