f859fdd4e7b876689fe9c1ed966b24dde5d14eea
Antoine629 <antoine.carrel@epfl.ch>
2025-03-11 16:45:48 +0100

    quaternion

--- ManeuverStabilization_StateEstimation/mainModels/funktionEKF.m
+++ ManeuverStabilization_StateEstimation/mainModels/funktionEKF.m
@@ -1,5 +1,5 @@
-function [outputArg1,outputArg2] = untitled(inputArg1,inputArg2)
-%UNTITLED Summary of this function goes here
+function [xk1] = funktionEKF(xk,uk)
+%FUNKTIONEKF Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
