f0b0f6ef12819e258bf0e56fcf57aad6eb37d71e
aero309 <khanhky@outlook.com>
2025-03-31 01:31:29 +0200

    recovery from 45deg

--- ManeuverStabilization_Sliding_mode/utilities/startVars.m
+++ ManeuverStabilization_Sliding_mode/utilities/startVars.m
@@ -36,10 +36,10 @@ thrustArm = 0.10795;
 % Initial conditions
 init.date = [2017 1 1 0 0 0];
 init.posLLA = [42.299886 -71.350447 71.3232];
-init.posNED = [57 95 -0.046-1.5];
-init.vb = [-3 -3 0];
-init.euler = [0 0 0];
-init.angRates = [0 0 -5];
+init.posNED = [57 95 -0.0460-1.5];
+init.vb = [0 0 0];
+init.euler = [pi/4 0 0];
+init.angRates = [0 0 0];
 
 % Initialize States:
 States = Simulink.Bus.createMATLABStruct('StatesBus');
