41a1681665a2980814094a369c5896f978d3ad28
Aloismi <alois.millot@epfl.ch>
2025-05-07 14:16:49 +0200

    P PI tuning

--- ManeuverStabilization_AltitudeEstimation/MeasurementJacobianFcn.m
+++
@@ -1,4 +0,0 @@
-function H = MeasurementJacobianFcn(~)
-    % Measurement Jacobian for EKF
-    H = [1, 0, 0]; 
-end
--- ManeuverStabilization_AltitudeEstimation/StateJacobianFcn.m
+++
@@ -1,7 +0,0 @@
-function F = StateJacobianFcn(~,~)
-    % State Transition Jacobian for EKF
-    Ts = 0.005;
-    F = [1, Ts,  0;
-        0,  1, -Ts;
-        0,  0,  1];
-end
